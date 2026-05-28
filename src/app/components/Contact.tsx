252fd96323c06d30700ebd02edee1054859e181d
Rohit Kartik <81758520+RKartik9@users.noreply.github.com>
2026-05-28 17:04:52 +0530

    feat: update resume

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -508,7 +508,7 @@ const Contact = () => {
             </div>
             <div className="flex items-center gap-6">
               <motion.a
-                href="https://drive.google.com/file/d/1PsjNbAFqbi-lh7TdDTKp3OzaBVIH-aGK/view?usp=sharing"
+                href="https://drive.google.com/file/d/1J-pS1nYvAz0Vxd0jhVVqPzo5EyD3xOz-/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 whileHover={{ scale: 1.05 }}
