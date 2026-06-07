9f958e29f6a8d8e4ce934187d1d41b66f34b5bb7
Rohit Kartik <81758520+RKartik9@users.noreply.github.com>
2026-06-07 22:06:40 +0530

    chore: update resume

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -508,7 +508,7 @@ const Contact = () => {
             </div>
             <div className="flex items-center gap-6">
               <motion.a
-                href="https://drive.google.com/file/d/1J-pS1nYvAz0Vxd0jhVVqPzo5EyD3xOz-/view?usp=sharing"
+                href="https://drive.google.com/file/d/1ZTTgrskxm5K3_cAfsYO0ZZ0MJN66c3pw/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 whileHover={{ scale: 1.05 }}
--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -366,7 +366,7 @@ const Hero = () => {
               whileTap={{ scale: 0.95 }}
               onClick={() =>
                 window.open(
-                  "https://drive.google.com/file/d/1J-pS1nYvAz0Vxd0jhVVqPzo5EyD3xOz-/view?usp=sharing",
+                  "https://drive.google.com/file/d/1ZTTgrskxm5K3_cAfsYO0ZZ0MJN66c3pw/view?usp=sharing",
                   "_blank"
                 )
               }
